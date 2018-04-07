b34bef13e8932a0a332b04c094a56fe81daa8e64
Baris Aydek <baris.aydek@comtravo.com>
2018-04-07 22:23:46 +0200

    removed html from index

--- src/indexer.js
+++ src/indexer.js
@@ -45,18 +45,26 @@ const items = chain(thr => {
     id: thr.id,
     url: thr.thread_url,
     text: content.text,
-    html: content.html,
     parent: null,
+    next: content.children.length > 0
+      ? content.children[0].id
+      : null,
     thread_index: thr.index,
     thread_desc: thr.desc
   }].concat(
     content.children.map((child, index) => 
-      merge(child, { 
+      merge({
+        id: child.id,
+        url: child.url,
+        text: child.text
+      }, { 
         parent: index === 0 
           ? thr.id 
-          : content.children[index - 1].id 
-        }
-      )
+          : content.children[index - 1].id,
+        next: index < content.children.length - 1
+          ? content.children[index + 1].id
+          : null
+      })
     )
   );
 }, threads);
@@ -81,10 +89,13 @@ var index = elasticlunr(function () {
 
 console.log('creating index file...');
 
-
 writeFile('./index.elastic.nospace.json', index)
   .then(() => { 
+    const tevrat = index.search('tevrat')[0];
+    const bestEntry = index.documentStore.docs[tevrat.ref];
+
     console.log('index file is created, please copy to content folder');
-    console.log(util.inspect(index.search('tevrat')[0], false, null));
+    console.log(tevrat);
+    console.log(util.inspect(bestEntry, false, null));
   })
   .catch(console.error);
